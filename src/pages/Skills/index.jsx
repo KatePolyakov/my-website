ad3d66104734fb55621052f787970b7258cf085e
Kate <polyakovkate@gmail.com>
2024-01-19 15:56:36 -0800

    added skills section

--- src/pages/Skills/index.jsx
+++ src/pages/Skills/index.jsx
@@ -12,7 +12,50 @@ const SkillsPage = () => {
           <Icon icon="cogs" size={30} color="#6EA3AB" />
           Skills
         </h2>
-        
+        <div className={classes.skills__group}>
+          <button>
+            <Icon icon="html5" size={45} />
+          </button>
+          <button>
+            <Icon icon="csswizardry" size={45} />
+          </button>
+          <button>
+            <Icon icon="sass" size={45} />
+          </button>
+          <button>
+            <Icon icon="javascript" size={45} />
+          </button>
+          <button>
+            <Icon icon="react" size={45} />
+          </button>
+          <button>
+            <Icon icon="typescript" size={45} />
+          </button>
+          <button>
+            <Icon icon="mongodb" size={45} />
+          </button>
+          <button>
+            <Icon icon="mysql" size={45} />
+          </button>
+          <button>
+            <Icon icon="node-dot-js" size={45} />
+          </button>
+          <button>
+            <Icon icon="bootstrap" size={45} />
+          </button>
+          <button>
+            <Icon icon="figma" size={45} />
+          </button>
+          <button>
+            <Icon icon="icomoon" size={45} />
+          </button>
+          <button>
+            <Icon icon="auth0" size={45} />
+          </button>
+          <button>
+            <Icon icon="github1" size={45} />
+          </button>
+        </div>
       </div>
     </div>
   );
